b25f5739fa4fdc94bb76a38facd8562744864b0e
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2025-05-21 10:46:20 -0400

    CSVt

--- IdentityPlotPlugin.R
+++ IdentityPlotPlugin.R
@@ -10,7 +10,9 @@ run <- function() {
 }
 
 output <- function(outputfile) {
-ggplot(Plot.Distribution, aes(x = Genome_sizeM, fill = Superkingdom)) +
+#print(str(Plot.Distribution))
+write.csv(Plot.Distribution$genome_sizeM, paste(outputfile, "csv", sep="."))
+	ggplot(Plot.Distribution, aes(x = Genome_sizeM, fill = Superkingdom)) +
   geom_histogram(position="identity", alpha = 0.7, binwidth = 0.1) +
   scale_x_continuous(limits = c(0,11), breaks = seq(0, 11, 1)) +
   ylim(0,500) + theme_bw()
